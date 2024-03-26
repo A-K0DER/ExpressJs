edc795ac91de86ab576e71cdbaa041c7765536ea
Ayush Kumar <ayushspidey@gmail.com>
2024-03-26 15:13:05 +0530

    changes

--- nodeServer.ts
+++ nodeServer.ts
@@ -3,15 +3,23 @@
 
  //http has create server method which takes a callback function as an argument, the call back fn takes two arguments request(req) and response(res)
  const server = http.createServer((req : any, res : any) => {
-     
-    // http msg
-    // 1. Start Line --> taken care by node.
-    // 2. header --> we need to take care.
-        res.writeHead(200, {'content-type': 'text/html'}); // takes 2 arg (status code, contentType(mime-type))
+     if(req.url === '/')
+     {
+        // http msg
+            // 1. Start Line --> taken care by node.
+            // 2. header --> we need to take care.
+            res.writeHead(200, {'content-type': 'text/html'}); // takes 2 arg (status code, contentType(mime-type))
 
-    // 3. body --> we neeed to do.
-        res.write('<h1> Hello World</h1>');
-        res.end(); // for closing the connection
+            // 3. body --> we neeed to do.
+            res.write('<h1> Welcome to Home Page</h1>');
+            res.end(); // for closing the connection
+     }
+     else
+     {
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.write('<h4>Not the Page you are looking for</h4>');
+        res.end();
+     }
  });
 
  server.listen(3000);
